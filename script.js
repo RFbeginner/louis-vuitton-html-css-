4361d839662f082f27d463797acb4e5d48cc348b
RFbeginner <arikferreiradealimeida@gmail.com>
2025-03-06 11:39:18 -0300

    11:38/06032025

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var img = document.getElementById("On to go Coleção");
+    if (window.innerWidth <= 768) {
+        img.src = "bannermobile.jpeg";
+    }
+});
