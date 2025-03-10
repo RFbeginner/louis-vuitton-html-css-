ea2722bdecf4e7994193368386248539cf9da991
RFbeginner <arikferreiradealimeida@gmail.com>
2025-03-10 11:24:32 -0300

    11:24/10032025

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var img = document.getElementById("Ontogocolecao");
-    if (window.innerWidth <= 768) {
-        img.src = "bannermobile.jpeg";
-    }
-});
